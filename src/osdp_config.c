08b8bcef4891f86d2e4e78f3d28a38cd895a1bc4
Jürgen Buschle <j.buschle@quantus-informatics.de>
2025-06-12 12:04:51 +0200

    variables to change from outside

---
+++ src/osdp_config.c
@@ -0,0 +1,6 @@
+#include "osdp_config.h"
+
+int osdp_resp_tout_ms_config = 200;
+int osdp_cmd_max_retries_config = 8;
+int osdp_online_retry_wait_max_ms_config = 300 * 1000;
+int osdp_cmd_retry_wait_ms_config = 800;
